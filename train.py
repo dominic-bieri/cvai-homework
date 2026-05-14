d1c3b04afa4f3f479630d6c669e11ae4a96c56b6
Dominic Bieri <schaeume-fabeln0t@icloud.com>
2026-05-14 11:14:50 +0200

    :sparkles: add main method for train.py

--- train.py
+++ train.py
@@ -1,25 +1,29 @@
 import torch
 from ultralytics import YOLO
 
-if torch.cuda.is_available():
-    print("NVIDIA GPU (CUDA) detected!")
-    compute_device = 'cuda:0'
-elif torch.backends.mps.is_available():
-    print("Apple Silicon GPU (MPS) detected!")
-    compute_device = 'mps'
-else:
-    print("No compatible GPU found, defaulting to CPU.")
-    compute_device = 'cpu'
+def main():
+    if torch.cuda.is_available():
+        print("NVIDIA GPU (CUDA) detected!")
+        compute_device = 'cuda:0'
+    elif torch.backends.mps.is_available():
+        print("Apple Silicon GPU (MPS) detected!")
+        compute_device = 'mps'
+    else:
+        print("No compatible GPU found, defaulting to CPU.")
+        compute_device = 'cpu'
 
-model = YOLO('yolo26n.pt')
+    model = YOLO('yolo26n.pt')
 
-print(f"Starting YOLO local training on {compute_device}...")
+    print(f"Starting YOLO local training on {compute_device}...")
 
-results = model.train(
-    data="self-labled-dataset-yolo/data.yaml",
-    epochs=150,
-    imgsz=1280,
-    device=compute_device,
-    project="local_bee_models",
-    name="yolo26_run_01",
-)
+    model.train(
+        data="self-labled-dataset-yolo/data.yaml",
+        epochs=1,
+        imgsz=1280,
+        device=compute_device,
+        project="local_bee_models",
+        name="yolo26_run_01",
+    )
+
+if __name__ == "__main__":
+    main()
